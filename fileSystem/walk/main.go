891a34888d94b02095cfc3c2367ecf759bb065d1
teakaysea <teakaysea@gmail.com>
2022-02-21 08:44:50 +0900

    add del function

--- fileSystem/walk/main.go
+++ fileSystem/walk/main.go
@@ -15,6 +15,8 @@ type config struct {
 	size int64
 	// list files
 	list bool
+	// delete files
+	del bool
 }
 
 func main() {
@@ -22,6 +24,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete files only")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
@@ -31,6 +34,7 @@ func main() {
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
 	}
 	if err := run(*root, os.Stdout, c); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -51,6 +55,11 @@ func run(root string, out io.Writer, cfg config) error {
 		// If list was explicitly set, don't do anything else
 		if cfg.list {
 			return listFile(path, out)
+		}
+
+		// Delete files
+		if cfg.del {
+			return delFile(path)
 		}
 
 		// List is the default option if nothing else was set
--- fileSystem/walk/main_test.go
+++ fileSystem/walk/main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -12,7 +15,7 @@ func TestRun(t *testing.T) {
 		cfg      config
 		expected string
 	}{
-		{"NoFilter", "testdata", config{"", 0, true}, "testdata/dir.log\ntestdata/dir2/script.sh\n"},
+		{"NoFilter", "testdata", config{ext: "", size: 0, list: true}, "testdata/dir.log\ntestdata/dir2/script.sh\n"},
 		{"FilterExtensionMatch", "testdata", config{ext: ".log", size: 0, list: true}, "testdata/dir.log\n"},
 		{"FilterExtensionSizeMatch", "testdata", config{ext: ".log", size: 10, list: true}, "testdata/dir.log\n"},
 		{"FilterExtensionSizeNoMatch", "testdata", config{ext: ".log", size: 20, list: true}, ""},
@@ -33,4 +36,68 @@ func TestRun(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRunDelExtension(t *testing.T) {
+	testCases := []struct {
+		name        string
+		cfg         config
+		extNoDelete string
+		nDelete     int
+		nNoDelete   int
+		expected    string
+	}{
+		{},
+		{"DeleteExtensionNoMatch", config{ext: ".log", del: true}, ".gz", 0, 10, ""},
+		{"DeleteExtensionMatch", config{ext: ".log", del: true}, "", 10, 0, ""},
+		{"DeleteExtensionMixed", config{ext: ".log", del: true}, ".gz", 5, 5, ""}}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var buf bytes.Buffer
+
+			tempDir, cleanup := createTempDir(t, map[string]int{
+				tc.cfg.ext:     tc.nDelete,
+				tc.extNoDelete: tc.nNoDelete,
+			})
+			defer cleanup()
+
+			if err := run(tempDir, &buf, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			res := buf.String()
+
+			if tc.expected != res {
+				t.Errorf("Expected %q, got %q instead\n", tc.expected, res)
+			}
+
+			filesLeft, err := os.ReadDir(tempDir)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(filesLeft) != tc.nNoDelete {
+				t.Errorf("Expected %d files left, go %d instead\n", tc.nNoDelete, len(filesLeft))
+			}
+		})
+	}
+}
+func createTempDir(t *testing.T, files map[string]int) (dirname string, cleanup func()) {
+	t.Helper()
+
+	tempDir, err := os.MkdirTemp("", "walktest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for k, n := range files {
+		for i := 1; i <= n; i++ {
+			fname := fmt.Sprintf("file%d%s", i, k)
+			fpath := filepath.Join(tempDir, fname)
+			if err := os.WriteFile(fpath, []byte("dummy"), 0644); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	return tempDir, func() { os.RemoveAll(tempDir) }
 }
